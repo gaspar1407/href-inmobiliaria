c97e4d73665294b19b8a43a7dd9617452e639711
markon1i <markoschneider34@gmail.com>
2022-10-03 16:04:47 -0300

    DB

--- api/src/models/Propiedades.js
+++ api/src/models/Propiedades.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "propiedades",
+    "Propiedades",
     {
       id: {
         type: DataTypes.INTEGER,

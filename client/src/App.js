1b4e6ca7d783d8f55b5702f0545195c840962e84
gaspar <gasparmunoz@hotmail.com>
2022-10-04 14:38:56 -0300

    Instale Redux y deje client lista para codear

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
